c176f6e95f0942016f47434f1a3677168de89d88
Roksana Półtorak <poltorak.roksana@gmail.com>
2018-06-12 14:39:55 +0200

    initial commit

--- Zadanie_dodatkowe.py
+++ Zadanie_dodatkowe.py
@@ -1,15 +1,15 @@
-records = "test_user_login,14.0,success\ntest_user_login,1.0,failure\ntest_display_devices,15.3,success\ntest_display_devices,3.1,failure\ntest_user_logout,1.2,success\ntest_show_alerts,10.0,success"
+records = "test_user_login,16.0,success\ntest_user_login,1.0,failure\ntest_display_devices,15.3,success\ntest_display_devices,3.1,failure\ntest_user_logout,1.2,success\ntest_show_alerts,10.0,success"
 
 b = records.split('\n')
-lst = []
+new_b = []
 
 for i in range(0, len(b)):
-    lst.append(b[i].split(','))
+    new_b.append(b[i].split(','))
 
 success = {}
 failure = {}
 
-for i in lst:
+for i in new_b:
     for j in i:
         if j == 'success':
             success[i[0]] = success.get('success', 0) + 1
@@ -21,21 +21,21 @@ print('Failure tests: ', failure)
 
 times = {}
 
-for i in lst:
+for i in new_b:
     for j in i:
         if j == 'success':
             times[i[0]] = (i[1])
 
 print('Times of successful tests: ', times)
 
-new_list1 = []
-new_list = []
+names_of_tests = []
+times_of_tests = []
 
 for name, time in times.items():
-    new_list1.append(name)
-    new_list.append(time)
+    names_of_tests.append(name)
+    times_of_tests.append(time)
 
-a = max(new_list)
-c = new_list.index(a)
+time_max = max(times_of_tests)
+test_max = times_of_tests.index(time_max)
 
-print('The longest test was ', new_list1[c], ' and lasted ', a, 'seconds.')
+print('The longest test was ', names_of_tests[test_max], ' and lasted ', time_max, 'seconds.')
